99d00fc4df4b25fe70a79b70268f033b105dbcd4
Phil Hanna <ph1204@gmail.com>
2023-07-18 05:42:48 -0400

    Formatted source code

--- src/test/java/com/philhanna/diff/BaseTest.java
+++ src/test/java/com/philhanna/diff/BaseTest.java
@@ -19,59 +19,63 @@ import org.junit.Before;
  * Abstract class that finds the test data directory
  */
 public abstract class BaseTest {
- 
-   public static final String TEST_DIRECTORY = "/testdata";
-   public URL testDirectory = null;
-   
+
+    public static final String TEST_DIRECTORY = "/testdata";
+    public URL testDirectory = null;
+
    @Before
    public void setUp() throws Exception {
       testDirectory = getClass().getResource(TEST_DIRECTORY);
       assertNotNull(testDirectory);
    }
 
-   @After
-   public void tearDown() throws Exception {
-      testDirectory = null;
-   }
+    @After
+    public void tearDown() throws Exception {
+        testDirectory = null;
+    }
 
-   /**
-    * Loads the lines of text in the specified file into a list of strings
-    * @param name the name of the file in the test directory
-    * @return a list of strings
-    * @throws IOException if an I/O error occurs
-    */
-   protected List<String> fileToList(File fileName) throws IOException {
-      final InputStream inputStream = new FileInputStream(fileName);
-      return fileToList(inputStream);
-   }
+    /**
+     * Loads the lines of text in the specified file into a list of strings
+     * 
+     * @param name the name of the file in the test directory
+     * @return a list of strings
+     * @throws IOException if an I/O error occurs
+     */
+    protected List<String> fileToList(File fileName) throws IOException {
+        final InputStream inputStream = new FileInputStream(fileName);
+        return fileToList(inputStream);
+    }
 
-   /**
-    * Loads the lines of text in the specified file into a list of strings
-    * @param name the name of the file in the test directory
-    * @return a list of strings
-    * @throws IOException if an I/O error occurs
-    */
-   protected List<String> urlToList(URL url) throws IOException {
-      final InputStream inputStream = url.openStream();
-      return fileToList(inputStream);
-   }
+    /**
+     * Loads the lines of text in the specified file into a list of strings
+     * 
+     * @param name the name of the file in the test directory
+     * @return a list of strings
+     * @throws IOException if an I/O error occurs
+     */
+    protected List<String> urlToList(URL url) throws IOException {
+        final InputStream inputStream = url.openStream();
+        return fileToList(inputStream);
+    }
 
-   /**
-    * Loads the lines of text in the specified file into a list of strings
-    * @param file an input file
-    * @return a list of strings
-    * @throws IOException if an I/O error occurs
-    */
-   protected List<String> fileToList(InputStream inputStream) throws IOException {
-      final List<String> list = new ArrayList<String>();
-      final BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-      for (;;) {
-         final String line = in.readLine();
-         if (line == null) break;
-         list.add(line);
-      }
-      in.close();
-      return list;
-   }
+    /**
+     * Loads the lines of text in the specified file into a list of strings
+     * 
+     * @param file an input file
+     * @return a list of strings
+     * @throws IOException if an I/O error occurs
+     */
+    protected List<String> fileToList(InputStream inputStream) throws IOException {
+        final List<String> list = new ArrayList<String>();
+        final BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+        for (;;) {
+            final String line = in.readLine();
+            if (line == null)
+                break;
+            list.add(line);
+        }
+        in.close();
+        return list;
+    }
 
 }
